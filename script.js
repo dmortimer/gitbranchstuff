09ebc9ef757293d79c58064150cd7f323e3c1114
Dylan Mortimer <dmortimer10@gmail.com>
2017-05-09 13:30:12 -0400

    fizzbuzz

---
+++ script.js
@@ -0,0 +1,16 @@
+for(var i = 0; i < 30; i++) {
+
+    if(i % 5 === 0)
+
+}
+for(var i = 0; i < 30; i++) {
+
+    if(i % 5 === 0)
+
+}
+for(var i = 0; i < 30; i++) {
+
+    if(i % 5 === 0)
+
+}
+FIZZBUZZ!
